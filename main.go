c098075d566afa6da739e16beaa6cfa5b9b7813e
Andrew Meiling <ameiling@circadence.com>
2018-06-14 08:04:44 -0700

    setup cards

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	cards := []string{newCard(), newCard()}
+
+	cards = append(cards, "Six of Spades")
+
+	fmt.Println(cards)
+}
+
+func newCard() string {
+	return "Five of Dimonds"
+}
